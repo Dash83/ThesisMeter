603f38b7ffa2992a8a0337e561f668a076e3b79a
Marco Caballero <caballeromarco@gmail.com>
2020-04-16 18:21:34 +0100

    * Modified PATH to find my local version of python first. * Added comments to the config sample for users to fill more easily.

--- thesis_meter_job.sh
+++ thesis_meter_job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Set a PATH manually
-export PATH=/usr/local/bin:/usr/local/sbin:~/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Library/TeX/texbin/:/usr/local/opt/python/libexec/bin/
+export PATH=/usr/local/bin:/usr/local/sbin:~/bin:/usr/local/opt/python/libexec/bin/python:/usr/bin:/bin:/usr/sbin:/sbin:/Library/TeX/texbin/
 
 # Import configurations
 source config
@@ -10,9 +10,7 @@ export d=$(date "+%d/%m/%y")
 export count=$(detex "$MAIN_THESIS_FILE" | wc -w | xargs)
 echo "$d,$count" >> "$DATA_FILE"
 
-#Produce the figures
 #Word count figure
-#echo $python_bin "$plot_script" "$output_file" 
 python "$PLOT_SCRIPT" "$DATA_FILE" "$FIG_WORD_COUNT" 
 
 #TODO: word delta figure
@@ -23,7 +21,11 @@ python "$PLOT_SCRIPT" "$DATA_FILE" "$FIG_WORD_COUNT"
 cd $REPO_DIR
 git add *
 git commit -a -m "Daily data update"
-#git push origin master
+# Push command using user/pwd authentication.
 git push --repo https://$GIT_USER:$GIT_PWD@github.com/$GIT_USER/ThesisMeter.git
+# Git command using default authentication (works when ssh keys have been setup)
+# Use this commmand instead of the one above if you don't feel confortable writing
+# your github password in the config file.
+# git push origin master
 
 echo "ThesisMeter job ran OK!"
