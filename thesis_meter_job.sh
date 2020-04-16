3a08983e70cd79228454df774e591fd37dcda9ca
Marco Caballero <caballeromarco@gmail.com>
2020-04-16 17:51:22 +0100

    Daily data update

--- thesis_meter_job.sh
+++ thesis_meter_job.sh
@@ -1,38 +1,29 @@
 #!/bin/bash
+#Set a PATH manually
+export PATH=/usr/local/bin:/usr/local/sbin:~/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Library/TeX/texbin/:/usr/local/opt/python/libexec/bin/
 
-####################################
-# Set these variables appropriately
-####################################
-thesis_dir="/Users/marco/Documents/Github/Thesis"
-repo_dir="/Users/marco/Documents/Github/ThesisMeter"
-python_bin="/usr/local/opt/python/libexec/bin/python"
-# The main thesis file that includes all others
-main_thesis_file="$thesis_dir/thesis.tex"
-# The data file in which statistics will be appended every day
-data_file="$repo_dir/data.csv"
-# The path of the figure that will be produced
-output_file="$repo_dir/word_count.png"
-# Path to the script that plots the results. It's in the repo.
-plot_script="$repo_dir/plot_word_count.py"
+# Import configurations
+source config
 
 #Extract the daily data
 export d=$(date "+%d/%m/%y")
-export count=$(detex "$main_thesis_file" | wc -w | xargs)
-echo "$d,$count" >> "$data_file"
+export count=$(detex "$MAIN_THESIS_FILE" | wc -w | xargs)
+echo "$d,$count" >> "$DATA_FILE"
 
 #Produce the figures
 #Word count figure
 #echo $python_bin "$plot_script" "$output_file" 
-$python_bin "$plot_script" "$data_file" "$output_file" 
+python "$PLOT_SCRIPT" "$DATA_FILE" "$FIG_WORD_COUNT" 
 
 #TODO: word delta figure
 
 #TODO: Figure-count figure
 
 #Commit changes to repo
-cd $repo_dir
+cd $REPO_DIR
 git add *
 git commit -a -m "Daily data update"
-git push origin master
+#git push origin master
+git push --repo https://$GIT_USER:$GIT_PWD@github.com/$GIT_USER/ThesisMeter.git
 
 echo "ThesisMeter job ran OK!"
