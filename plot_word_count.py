67a49862bb334659f6ccd2dfd96ff35b328e1ccd
Marco Caballero <caballeromarco@gmail.com>
2020-11-28 19:55:00 +0000

    Daily data update

--- plot_word_count.py
+++ plot_word_count.py
@@ -3,18 +3,23 @@ import matplotlib.pyplot as plt
 import sys
 import os.path
 import numpy as np
+import calendar
 
 def usage():
     print("python plot_word_count.py DATA_FILE OUTPUT_FILE")
 
 def main(data_file, output_file):
     ds = pd.read_csv(data_file)
+
+    #Calculations to display only months in the xticks
     dates = pd.to_datetime( ds["Date"])
     num_entries = len(dates)
-    months = dates.dt.month_name().unique()
+    months = dates.dt.month
+    months = months.apply(lambda x: calendar.month_abbr[x]).unique()    
     num_months = len(months)
     step = num_entries / num_months
     ticks = np.arange(0, num_entries, step=step)
+
     x_axis=ds.axes[1][0]
     y_axis=ds.axes[1][1]
     plt.plot(ds[x_axis], ds[y_axis])
